c865943fd0bee90f24d4e193acba3c1ab483e7e9
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2024-01-26 19:03:15 +0500

    muzzammik

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import {  useDispatch } from 'react-redux'
+import { addTodo } from '../feature/todo/todoSlice';
+const AddTodo = () => {
+    const [input, setInput] = useState("")
+    const dispatch = useDispatch();
+    const addTodoHandler = (e) => {
+        e.preventDefault();
+        dispatch(addTodo(input))
+        setInput('')
+    }
+  return (
+   <form onSubmit={addTodoHandler}>
+    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='' />
+    <button>Submit</button>
+   </form>
+  )
+}
+
+export default AddTodo
